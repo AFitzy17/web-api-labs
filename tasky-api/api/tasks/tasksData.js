8a39ba7cb172506c018e289e1001688823a53928
AFitzy17 <alan.fitzgerald96@gmail.com>
2025-05-25 16:58:36 +0100

    Mongo lab Error handling

--- tasky-api/api/tasks/tasksData.js
+++ tasky-api/api/tasks/tasksData.js
@@ -1,18 +1,8 @@
 export const tasksData = {
     page: 1,
     total_pages: 1,
-    total_results: 4,
+    total_results: 3,
     tasks: [{ id: "1", title: "Dishes", description: "Empty dishwasher", deadline: "Today", done: false, priority: "low" },
     { id: "2", title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false, priority: "medium" },
-    { id: "3", title: "Tidy up", deadline: "Today", done: false, priority: "high" },
-{
-    "id": "4",
-    "title": "Doctor Appt",
-    "description": "Dr, Finbeter",
-    "deadline": "tomorrow",
-    "priority": "medium",
-    "done": "false",
-    "created_at": "2025-05-25T15:01:00.718Z",
-    "updated_at": "2025-05-25T15:01:00.718Z"
-}]
+    { id: "3", title: "Tidy up", deadline: "Today", done: false, priority: "high" }
 };
