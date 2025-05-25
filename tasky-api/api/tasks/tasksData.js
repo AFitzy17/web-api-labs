f5a264a5ab60ee9ebcbddb73cd0ce8c66793759e
AFitzy17 <alan.fitzgerald96@gmail.com>
2025-05-25 16:05:01 +0100

    Express Lab - exercise done

--- tasky-api/api/tasks/tasksData.js
+++ tasky-api/api/tasks/tasksData.js
@@ -1,8 +1,18 @@
 export const tasksData = {
     page: 1,
     total_pages: 1,
-    total_results: 3,
+    total_results: 4,
     tasks: [{ id: "1", title: "Dishes", description: "Empty dishwasher", deadline: "Today", done: false, priority: "low" },
     { id: "2", title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false, priority: "medium" },
-    { id: "3", title: "Tidy up", deadline: "Today", done: false, priority: "high" }]
+    { id: "3", title: "Tidy up", deadline: "Today", done: false, priority: "high" },
+{
+    "id": "4",
+    "title": "Doctor Appt",
+    "description": "Dr, Finbeter",
+    "deadline": "tomorrow",
+    "priority": "medium",
+    "done": "false",
+    "created_at": "2025-05-25T15:01:00.718Z",
+    "updated_at": "2025-05-25T15:01:00.718Z"
+}]
 };
